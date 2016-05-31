3df86de6f1edb00a1d2d6903a798106d95956960
mcollazosm <zsnes19.mc@gmail.com>
2016-05-31 10:24:30 -0500

    documento epf

---
+++ epf/Proyecto SW2/scripts/processElementData.js
@@ -0,0 +1,22 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_3OAoQSYXEeaa7sxwnPz2yQ,_D6dFsSYYEeaa7sxwnPz2yQActivity"]="./../../plugin/deliveryprocesses/resources/realizar casos de uso_30760C20_e4f2d2b_Activity.jpeg"
+contentPage.processPage.imageFiles["_3OAoQSYXEeaa7sxwnPz2yQ,_D6dFsSYYEeaa7sxwnPz2yQActivityDetail"]="./../../plugin/deliveryprocesses/resources/realizar casos de uso_30760C20_e4f2d2b_ActivityDetail.jpeg"
+contentPage.processPage.elementUrls["_LYtnACYYEeaa7sxwnPz2yQ"] = ["Documento Caso De Uso","plugin/workproducts/documento_caso_de_uso_C69E41AA.html"];
+contentPage.processPage.elementUrls["_LYrx0SYYEeaa7sxwnPz2yQ"] = ["desarrollador","plugin/roles/desarrollador_5CC9C853.html"];
+contentPage.processPage.elementUrls["_L7m4sCYYEeaa7sxwnPz2yQ"] = ["realizar pruebas de caso de uso","plugin/tasks/realizar_pruebas_de_caso_de_uso_D223BACD.html"];
+contentPage.processPage.elementUrls["_D6dFsSYYEeaa7sxwnPz2yQ"] = ["realizacion y aprobacion de casos de uso","plugin/deliveryprocesses/realizar casos de uso_30760C20.html"];
+contentPage.processPage.elementUrls["_LYrx0CYYEeaa7sxwnPz2yQ"] = ["realizar implementacion de casos de uso","plugin/tasks/realizar_implementacion_de_casos_de_uso_CEE00E95.html"];
+contentPage.processPage.elementUrls["_3OAoQSYXEeaa7sxwnPz2yQ"] = ["procesos","plugin/deliveryprocesses/procesos_4C7287B7.html"];
+contentPage.processPage.elementUrls["_L7m4sSYYEeaa7sxwnPz2yQ"] = ["Pruebas de caso de uso","plugin/workproducts/pruebas_de_caso_de_uso_DA069FC1.html"];
